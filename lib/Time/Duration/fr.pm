d35c5607ddf0b16e03e2198ab2ca56303d1a13d9
Sebastien Aperghis-Tramoni <sebastien@aperghis.net>
2010-06-19 18:48:23 +0200

    Beef up the documentation with examples for each function.

--- lib/Time/Duration/fr.pm
+++ lib/Time/Duration/fr.pm
@@ -149,6 +149,8 @@ sub _render {
 
 __END__
 
+=encoding UTF-8
+
 =head1 NAME
 
 Time::Duration::fr - describe time duration in French
@@ -169,6 +171,131 @@ Version 0.01
 =head1 DESCRIPTION
 
 C<Time::Duration::fr> is a localized version of C<Time::Duration>.
+
+
+=head1 FUNCTIONS
+
+=over
+
+=item duration($seconds)
+
+=item duration($seconds, $precision)
+
+Returns English text expressing the approximate time duration
+of C<abs($seconds)>, with at most S<C<$precision || 2>> expressed units.
+
+Examples:
+
+    duration(130)       => "2 minutes et 10 secondes"
+
+    duration(243550)    => "2 jours et 20 heures"
+    duration(243550, 1) => "3 jours"
+    duration(243550, 3) => "2 jours, 19 heures, et 39 minutes"
+    duration(243550, 4) => "2 jours, 19 heures, 39 minutes, et 10 secondes"
+
+
+=item duration_exact($seconds)
+
+Same as C<duration($seconds)>, except that the returned value is an exact
+(unrounded) expression of C<$seconds>.
+
+Example:
+
+    duration_exact(31629659) => "1 année, 1 jour, 2 heures, et 59 secondes"
+
+
+=item ago($seconds)
+
+=item ago($seconds, $precision)
+
+=item ago_exact($seconds)
+
+Negative values are passed to C<from_now()> / C< from_now_exact()>.
+
+Examples:
+
+    ago(243550)         => "il y a 2 jours et 20 heures"
+    ago(243550, 1)      => "il y a 3 jours"
+    ago_exact(243550)   => "il y a 2 jours, 19 heures, 39 minutes, et 10 secondes"
+
+    ago(0)              => "maintenant"
+
+    ago(-243550)        => "dans 2 jours et 20 heures"
+    ago(-243550, 1)     => "dans 3 jours"
+
+
+=item from_now($seconds)
+
+=item from_now($seconds, $precision)
+
+=item from_now_exact($seconds)
+
+Negative values are passed to C<ago()> / C< ago_exact()>.
+
+Examples:
+
+    from_now(243550)    => "dans 2 jours et 20 heures"
+    from_now(243550, 1) => "dans 3 jours"
+
+    from_now(0)         => "maintenant"
+
+    from_now(-243550)   => "il y a 2 jours et 20 heures"
+    from_now(-243550, 1)=> "il y a 3 jours"
+
+
+=item later($seconds)
+
+=item later($seconds, $precision)
+
+=item later_exact($seconds)
+
+Negative values are passed to C<ago()> / C< ago_exact()>.
+
+Examples:
+
+    later(243550)       => "2 jours et 20 heures plus tard"
+    later(243550, 1)    => "3 jours plus tard"
+
+    later(0)            => "maintenant"
+
+    later(-243550)      => "2 jours et 20 heures plus tôt"
+    later(-243550, 1)   => "3 jours plus tôt"
+
+
+=item earlier($seconds)
+
+=item earlier($seconds, $precision)
+
+=item earlier_exact($seconds)
+
+Negative values are passed to C<ago()> / C< ago_exact()>.
+
+Examples:
+
+    earlier(243550)     => "2 jours et 20 heures plus tôt"
+    earlier(243550, 1)  => "3 jours plus tôt"
+
+    earlier(0)          => "maintenant"
+
+    earlier(-243550)    => "2 jours et 20 heures plus tard"
+    earlier(-243550, 1) => "3 jours plus tard"
+
+
+=item concise( I<function(> ... ) )
+
+C<concise()> takes the string output of one of the above functions
+and makes it more concise.
+
+Examples:
+
+    ago(4567)           => "il y a 1 heure et 16 minutes"
+    concise(ago(4567))  => "il y a 1h16m"
+
+    earlier(31629659)           => "1 année et 1 jour plus tôt"
+    concise(earlier(31629659))  => "1a1j plus tôt"
+
+
+=back
 
 
 =head1 SEE ALSO
